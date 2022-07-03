1499f86d1650bc8bb9ee9376e73f502def4f8470
aychang95 <aychang995@gmail.com>
2022-07-03 03:20:35 -0400

    Revert gen modules

--- src/godel/queries/GetAuthenticationMessage.py
+++ src/godel/queries/GetAuthenticationMessage.py
@@ -1,8 +1,8 @@
 import sgqlc.types
 import sgqlc.operation
-import dapp_api.schema
+import godel.schema
 
-_schema = dapp_api.schema
+_schema = godel.schema
 _schema_root = _schema.schema
 
 __all__ = ('Operations',)
--- src/godel/queries/Predicate.py
+++ src/godel/queries/Predicate.py
@@ -1,9 +1,9 @@
-from dapp_api.fragments.PredicateDetails import fragment_predicate_details
+from godel.fragments.PredicateDetails import fragment_predicate_details
 import sgqlc.types
 import sgqlc.operation
-import dapp_api.schema
+import godel.schema
 
-_schema = dapp_api.schema
+_schema = godel.schema
 _schema_root = _schema.schema
 
 __all__ = ('Operations',)
