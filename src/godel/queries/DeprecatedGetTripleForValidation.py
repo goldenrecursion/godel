f850ac7cea2bc0036540298325c2956ad55fd6a3
aychang95 <aychang995@gmail.com>
2022-10-04 14:33:08 -0400

    Add assignvalidation and deprecatedgettripleforvalidation

---
+++ src/godel/queries/DeprecatedGetTripleForValidation.py
@@ -0,0 +1,33 @@
+import sgqlc.types
+import sgqlc.operation
+import godel.schema
+
+_schema = godel.schema
+_schema_root = _schema.schema
+
+__all__ = ('Operations',)
+
+
+def query_deprecated_get_triple_for_validation():
+    _op = sgqlc.operation.Operation(_schema_root.query_type, name='DeprecatedGetTripleForValidation')
+    _op_unvalidated_triple = _op.unvalidated_triple()
+    _op_unvalidated_triple__as__Statement = _op_unvalidated_triple.__as__(_schema.Statement)
+    _op_unvalidated_triple__as__Statement.__fragment__(fragment_triple_widget())
+    _op_unvalidated_triple__as__Statement_subject = _op_unvalidated_triple__as__Statement.subject()
+    _op_unvalidated_triple__as__Statement_subject.__fragment__(fragment_entity_summary())
+    _op_unvalidated_triple__as__Statement_object_entity = _op_unvalidated_triple__as__Statement.object_entity()
+    _op_unvalidated_triple__as__Statement_object_entity.__fragment__(fragment_entity_summary())
+    _op_current_user = _op.current_user()
+    _op_current_user.remaining_skips()
+    _op_current_user_user_flags = _op_current_user.user_flags()
+    _op_current_user_user_flags_nodes = _op_current_user_user_flags.nodes()
+    _op_current_user_user_flags_nodes.flag()
+    return _op
+
+
+class Query:
+    deprecated_get_triple_for_validation = query_deprecated_get_triple_for_validation()
+
+
+class Operations:
+    query = Query
