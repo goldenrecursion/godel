a5d959550b1b8eafbc4cda7950aaddd607340b34
aychang95 <aychang995@gmail.com>
2022-11-19 03:36:41 -0500

    Update schema and queries

--- src/godel/queries/LiveView.py
+++ src/godel/queries/LiveView.py
@@ -10,10 +10,15 @@ __all__ = ('Operations',)
 
 
 def query_live_view():
-    _op = sgqlc.operation.Operation(_schema_root.query_type, name='LiveView')
-    _op_triples = _op.triples(first=20, condition={'validationStatus': 'ACCEPTED'}, order_by='DATE_ACCEPTED_DESC')
-    _op_triples_nodes = _op_triples.nodes()
-    _op_triples_nodes.__fragment__(fragment_triple_widget())
+    _op = sgqlc.operation.Operation(_schema_root.query_type, name='LiveView', variables=dict(first=sgqlc.types.Arg(_schema.Int, default=20), after=sgqlc.types.Arg(_schema.Cursor, default=None), condition=sgqlc.types.Arg(_schema.TripleCondition), orderBy=sgqlc.types.Arg(sgqlc.types.list_of(sgqlc.types.non_null(_schema.TriplesOrderBy)), default=('DATE_ACCEPTED_DESC',))))
+    _op_triples = _op.triples(first=sgqlc.types.Variable('first'), after=sgqlc.types.Variable('after'), condition=sgqlc.types.Variable('condition'), order_by=sgqlc.types.Variable('orderBy'))
+    _op_triples_page_info = _op_triples.page_info()
+    _op_triples_page_info.end_cursor()
+    _op_triples_page_info.has_previous_page()
+    _op_triples_page_info.has_next_page()
+    _op_triples_edges = _op_triples.edges()
+    _op_triples_edges_node = _op_triples_edges.node()
+    _op_triples_edges_node.__fragment__(fragment_triple_widget())
     return _op
 
 
